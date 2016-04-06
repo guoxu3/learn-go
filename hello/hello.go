de2964212a3bc238fc804b91af898435c4ac509d
GuoXu <guoxu3@qq.com>
2016-04-06 15:17:13 +0800

    modify

--- hello/hello.go
+++ hello/hello.go
@@ -2,15 +2,8 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"strings"
 )
 
 func main()  {
-	who := "World!"
-	if len(os.Args) > 1 {
-		who = strings.Join(os.Args[1:], " ")
-	}
-
 	fmt.Println("Hello World!")
 }
